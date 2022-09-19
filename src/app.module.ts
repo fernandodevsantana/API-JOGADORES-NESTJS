7950468fb58ee1d8420901e6ccac94ff23f6e8e7
fernandodevsantana <fernandosantana.dev@gmail.com>
2022-09-19 13:55:06 -0300

    feat: create connection in database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import {config} from 'dotenv'
+config()
+
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.bysac1u.mongodb.net/?retryWrites=true&w=majority`),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
